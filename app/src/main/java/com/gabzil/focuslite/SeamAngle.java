1fdc557ab23e7b48e384fb255c260ee76a187a23
gabziltech <sales@gabzil.com>
2016-08-26 13:51:36 +0530

    26-8-2016

--- app/src/main/java/com/gabzil/focuslite/SeamAngle.java
+++ app/src/main/java/com/gabzil/focuslite/SeamAngle.java
@@ -1,13 +1,27 @@
 package com.gabzil.focuslite;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SeamAngle extends Fragment {
+    Button start;
+    ImageView image;
+    String str;
+    float angle;
+    long speed;
+    private Spinner swingAngle, swingSpeed;
+    TextView t1;
+
     public SeamAngle() {
         // Required empty public constructor
     }
@@ -17,14 +31,59 @@ public class SeamAngle extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_seam_angle, container, false);
 
-//        ImageView img_animation = (ImageView) v.findViewById(R.id.img_animation);
-//
-//        img_animation.clearAnimation();
-//        RotateAnimation anim = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f,Animation.RELATIVE_TO_SELF, 0.5f);
-//        anim.setFillAfter(true);
-//        anim.setRepeatCount(0);
-//        anim.setDuration(10000);
-//        img_animation.startAnimation(anim);
+        start=(Button) v.findViewById(R.id.startanimation);
+        image=(ImageView) v.findViewById(R.id.animationView);
+        swingAngle = (Spinner) v.findViewById(R.id.spinnerangle);
+        swingSpeed = (Spinner) v.findViewById(R.id.spinnerspeed);
+        t1=(TextView) v.findViewById(R.id.in);
+
+        swingAngle.setSelection(9);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    angle = Float.parseFloat(String.valueOf(swingAngle.getSelectedItem()));
+                    if(angle == 0){
+                        t1.setText("No-Swing");
+                        t1.setVisibility(View.VISIBLE);
+                    }
+                    else if(angle > 0){
+                        t1.setText("In-Swing");
+                        t1.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        t1.setText("Out-Swing");
+                        t1.setVisibility(View.VISIBLE);
+                    }
+                    str = String.valueOf(swingSpeed.getSelectedItem());
+                    if(str.equals("Slow")){
+                        speed=5000;
+                    }else if(str.equals("Medium")){
+                        speed=2000;
+                    }else if(str.equals("Fast")){
+                        speed=500;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                // Step1 : create the  RotateAnimation object
+                RotateAnimation anim = new RotateAnimation(0f, angle, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+
+                // Step 2:  Set the Animation properties
+                anim.setFillAfter(true);
+                anim.setInterpolator(new LinearInterpolator());
+
+                //anim.setRepeatCount(Animation.RELATIVE_TO_SELF);
+                anim.setRepeatCount(Animation.ABSOLUTE);
+                anim.setDuration(speed);
+
+                // Step 3: Start animating the image
+                image.startAnimation(anim);
+
+            }
+        });
+
         return v;
     }
 
--- app/src/main/java/com/gabzil/focuslite/Speed.java
+++ app/src/main/java/com/gabzil/focuslite/Speed.java
@@ -1,12 +1,28 @@
 package com.gabzil.focuslite;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Speed extends Fragment {
+    TextView tv;
+    ProgressBar pBar;
+    int pStatus;
+    private Handler handler = new Handler();
+    EditText speed;
+    Button Go;
+    Double Speed;
+
     public Speed() {
         // Required empty public constructor
     }
@@ -14,6 +30,89 @@ public class Speed extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_speed, container, false);
+        View v = inflater.inflate(R.layout.fragment_speed, container, false);
+        tv = (TextView) v.findViewById(R.id.textView1);
+        pBar = (ProgressBar) v.findViewById(R.id.progressBar1);
+        speed = (EditText) v.findViewById(R.id.speed);
+        Go = (Button) v.findViewById(R.id.go);
+
+        Go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!Validation()){
+                    InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+                    pStatus = 0;
+                    pBar.setSecondaryProgress(0);
+                    Speed = Double.valueOf(speed.getText().toString().trim());
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            while (pStatus <= Speed) {
+                                handler.post(new Runnable() {
+
+                                    @Override
+                                    public void run() {
+                                        // TODO Auto-generated method stub
+                                        if (pStatus > 100)
+                                            pBar.setSecondaryProgress(pStatus-100);
+                                        else
+                                            pBar.setProgress(pStatus);
+
+                                        tv.setText(Speed + " KM/HR");
+                                        tv.setTextSize(15);
+                                    }
+                                });
+                                try {
+                                    Thread.sleep(50);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                pStatus++;
+                            }
+                        }
+                    }).start();
+                }
+            }
+        });
+        return v;
     }
+
+    private boolean Validation()
+    {
+        boolean error=false;
+        String strmsg= "Please Enter ";
+
+        if(speed.getText().toString().trim().length() == 0)
+        {
+            strmsg += ", Speed";
+            error = true;
+        }
+
+        if(error == true)
+        {
+            String replacedString =strmsg.replace("Please Enter ,", "Please Enter ");
+            ShowAlert(replacedString);
+        }
+        return  error;
+    }
+
+    public void ShowAlert(String msg){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder.setMessage(msg);
+
+        alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // cancel the alert box and put a Toast to the user
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show alert
+        alertDialog.show();
+    }
+
+
 }
